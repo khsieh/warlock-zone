9d1590ae7a614481b326fcfd3eeebf5dec5a48a7
Kevin Hsieh <kevin.b.hsieh@gmail.com>
2018-03-23 14:21:56 -0400

    added edit_profile component

--- src/app/components/edit-profile/edit-profile.component.ts
+++ src/app/components/edit-profile/edit-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal,NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-edit-profile',
@@ -8,11 +9,26 @@ import { NgbModal,NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class EditProfileComponent implements OnInit {
 
-  constructor(public activeModal: NgbActiveModal) { }
+
+  editUser: User;
+  new_password1: string;
+  new_password2: string;
+
+  constructor(public activeModal: NgbActiveModal)//, private editUser:User) 
+  { }
 
   ngOnInit() {
+      //this should be initalized by the current logged in user
+      this.editUser = new User();
+      this.editUser.$firstName = "Matt";
+      this.editUser.$lastName = "Warlock";
+      this.editUser.$email = "warlock@zone.org";
+    //   this.editUser.$password = "you're not allow to have this";
+      this.editUser.$username = "warloccZ0ne";
   }
   updateUser():void{
     console.log("Updating user information!");
+    console.log("check if current password is correct");
+    console.log("check if new passwords match up");
   }
 }
